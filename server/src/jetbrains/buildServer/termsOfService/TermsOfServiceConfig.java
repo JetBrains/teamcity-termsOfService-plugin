3ca4f4ee8588af9c7ef305de7646a3ad09b0af0a
dmitry.treskunov <dmitry.treskunov@jetbrains.com>
2017-12-27 18:51:11 +0100

    Inline TermsOfServiceConfig

--- server/src/jetbrains/buildServer/termsOfService/TermsOfServiceConfig.java
+++ server/src/jetbrains/buildServer/termsOfService/TermsOfServiceConfig.java
@@ -6,35 +6,24 @@ import jetbrains.buildServer.serverSide.BuildServerListener;
 import jetbrains.buildServer.serverSide.ServerPaths;
 import jetbrains.buildServer.serverSide.TeamCityProperties;
 import jetbrains.buildServer.serverSide.impl.FileWatcherFactory;
-import jetbrains.buildServer.termsOfService.TermsOfServiceManager.ExternalAgreementLink;
-import jetbrains.buildServer.util.*;
+import jetbrains.buildServer.util.EventDispatcher;
+import jetbrains.buildServer.util.FileUtil;
 import org.jdom.Element;
 import org.jdom.JDOMException;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import javax.annotation.concurrent.ThreadSafe;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-
-import static com.intellij.openapi.util.text.StringUtil.isNotEmpty;
-import static java.util.Collections.emptyMap;
+import java.util.function.Consumer;
 
 @ThreadSafe
 public class TermsOfServiceConfig {
     private static final String CONFIG_FILE = "terms-of-service-config.xml";
 
-    private final List<AgreementSettings> myAgreements = new ArrayList<>();
-
-    private final List<ExternalAgreementLink> externalAgreements = new ArrayList<>();
-    private Optional<GuestNoticeSettings> myGuestNotice = Optional.empty();
-
     private final File myConfigDir;
     private final File mySettingsFile;
+    private Consumer<Element> onChangeListener;
 
     public TermsOfServiceConfig(@NotNull EventDispatcher<BuildServerListener> myEvents,
                                 @NotNull ServerPaths serverPaths,
@@ -61,210 +50,24 @@ public class TermsOfServiceConfig {
     }
 
     @NotNull
-    public synchronized List<AgreementSettings> getAgreements() {
-        return new ArrayList<>(myAgreements);
-    }
-
-    @NotNull
-    public synchronized Optional<AgreementSettings> getAgreement(@NotNull String id) {
-        return myAgreements.stream().filter(a -> a.getId().equals(id)).findFirst();
-    }
-
-    @NotNull
-    public synchronized List<ExternalAgreementLink> getExternalAgreements() {
-        return externalAgreements;
-    }
-
-    @NotNull
-    public synchronized Optional<GuestNoticeSettings> getGuestNotice() {
-        return myGuestNotice;
+    public File getConfigFile(@NotNull String filename) {
+        return new File(myConfigDir, filename);
     }
 
     synchronized void loadSettings() {
         try {
             if (mySettingsFile.exists()) {
-                myAgreements.clear();
-                externalAgreements.clear();
                 Element parsed = FileUtil.parseDocument(mySettingsFile, false);
-                List agreementsEls = parsed.getChildren("agreement");
-
-                if (agreementsEls.isEmpty()) {
-                    TermsOfServiceLogger.LOGGER.debug("No 'agreement' elements are found in " + FileUtil.getCanonicalFile(mySettingsFile).getPath());
-                }
-
-                for (Object agreementEl : agreementsEls) {
-                    Element paramsElement = ((Element) agreementEl).getChild("parameters");
-                    Map<String, String> params = paramsElement == null ? emptyMap() : XmlUtil.readParameters(paramsElement);
-                    if (params.get("agreement-file") != null) {
-                        List<TermsOfServiceManager.Consent> consents = new ArrayList<>();
-                        Element consentsEl = ((Element) agreementEl).getChild("consents");
-                        if (consentsEl != null) {
-                            for (Object consent : consentsEl.getChildren("consent")) {
-                                Element consentEl = ((Element) consent);
-                                String id = consentEl.getAttributeValue("id");
-                                String text = consentEl.getAttributeValue("text");
-                                boolean checked = Boolean.parseBoolean(consentEl.getAttributeValue("checked"));
-                                if (isNotEmpty(id) && isNotEmpty(text)) {
-                                    consents.add(new ConsentSettings(id, text, checked));
-                                }
-                            }
-                        }
-                        AgreementSettings agreementSettings = new AgreementSettings(((Element) agreementEl).getAttributeValue("id"), params, consents);
-                        myAgreements.add(agreementSettings);
-                    }
-                }
-
-                for (Object agreementEl : parsed.getChildren("externalAgreementLink")) {
-                    externalAgreements.add(new ExternalAgreementLinkSettings(((Element) agreementEl).getAttributeValue("text"), ((Element) agreementEl).getAttributeValue("url")));
-                }
-
-                GuestNoticeSettings guestNoticeSettings = null;
-                Element guestNoticeEl = parsed.getChild("guest-notice");
-                if (guestNoticeEl != null) {
-                    Element paramsElement = guestNoticeEl.getChild("parameters");
-                    Map<String, String> params = paramsElement == null ? emptyMap() : XmlUtil.readParameters(paramsElement);
-                    if (params.get("agreement") != null && params.get("text") != null) {
-                        guestNoticeSettings = new GuestNoticeSettings(params.get("text"), params.get("agreement"));
-                    }
-                }
-                myGuestNotice = Optional.ofNullable(guestNoticeSettings);
-
-            }
-
-            if (myAgreements.isEmpty()) {
-                TermsOfServiceLogger.LOGGER.warn("No Terms of Service rules were found in " + FileUtil.getCanonicalFile(mySettingsFile).getPath());
+                onChangeListener.accept(parsed);
             }
         } catch (IOException | JDOMException e) {
             TermsOfServiceLogger.LOGGER.warnAndDebugDetails("Error while loading Terms Of Service settings from " + FileUtil.getCanonicalFile(mySettingsFile).getPath(), e);
         }
     }
 
-    class AgreementSettings {
-
-        private final String id;
-        private final Map<String, String> params;
-        private final List<TermsOfServiceManager.Consent> consents;
-
-        AgreementSettings(@NotNull String id, @NotNull Map<String, String> params, List<TermsOfServiceManager.Consent> consents) {
-            this.id = id;
-            this.params = params;
-            this.consents = consents;
-        }
-
-        @NotNull
-        public String getId() {
-            return id;
-        }
-
-        @NotNull
-        public String getVersion() {
-            return params.getOrDefault("version", "1");
-        }
-
-        @NotNull
-        public String getShortName() {
-            return StringUtil.notNullize(params.get("short-name"), "Terms of Service");
-        }
-
-        @NotNull
-        public String getFullName() {
-            return StringUtil.notNullize(params.get("full-name"), "Terms of Service");
-        }
-
-        @Nullable
-        public String getText() {
-            String agreementFileParam = params.get("agreement-file");
-            if (agreementFileParam == null) {
-                return null;
-            }
-            File agreementFile = new File(myConfigDir, agreementFileParam);
-            try {
-                return FileUtil.readText(agreementFile, "UTF-8");
-            } catch (IOException e) {
-                TermsOfServiceLogger.LOGGER.warnAndDebugDetails("Error while reading Terms Of Service agreement file from " + agreementFile, e);
-                throw new IllegalStateException("Error while reading Terms Of Service agreement file from " + agreementFile, e);
-            }
-        }
-
-        @Nullable
-        public String getLink() {
-            if (getText() != null) return null;
-            return params.get("agreement-link");
-        }
-
-        @NotNull
-        public List<TermsOfServiceManager.Consent> getConsents() {
-            return consents;
-        }
+    public void setOnChange(@NotNull Consumer<Element> listener) {
+        this.onChangeListener = listener;
     }
 
-    class ExternalAgreementLinkSettings implements ExternalAgreementLink{
-        private final String text;
-        private final String url;
-
-        ExternalAgreementLinkSettings(String text, String url) {
-            this.text = text;
-            this.url = url;
-        }
-
-        @NotNull
-        @Override
-        public String getName() {
-            return text;
-        }
-
-        @NotNull
-        @Override
-        public String getUrl() {
-            return url;
-        }
-    }
-
-    class GuestNoticeSettings {
-        private final String text;
-        private final String agreementId;
-
-        GuestNoticeSettings(String text, String agreementId) {
-            this.text = text;
-            this.agreementId = agreementId;
-        }
-
-        public String getText() {
-            return text;
-        }
-
-        public String getAgreementId() {
-            return agreementId;
-        }
-    }
-
-    class ConsentSettings implements TermsOfServiceManager.Consent {
-        @NotNull private final String id;
-        @NotNull private final String text;
-        private final boolean checked;
-
-        ConsentSettings(@NotNull String id, @NotNull String text, boolean checked) {
-            this.id = id;
-            this.text = text;
-            this.checked = checked;
-        }
-
-        @NotNull
-        @Override
-        public String getId() {
-            return id;
-        }
-
-        @Override
-        public boolean isCheckedByDefault() {
-            return checked;
-        }
-
-        @NotNull
-        @Override
-        public String getText() {
-            return text;
-        }
-    }
 }
 
--- server/src/jetbrains/buildServer/termsOfService/TermsOfServiceManagerImpl.java
+++ server/src/jetbrains/buildServer/termsOfService/TermsOfServiceManagerImpl.java
@@ -4,19 +4,21 @@ import jetbrains.buildServer.serverSide.TeamCityProperties;
 import jetbrains.buildServer.users.SUser;
 import jetbrains.buildServer.users.SimplePropertyKey;
 import jetbrains.buildServer.users.UserModel;
-import jetbrains.buildServer.util.TimeService;
-import jetbrains.buildServer.util.VersionComparatorUtil;
+import jetbrains.buildServer.util.*;
 import jetbrains.buildServer.web.util.WebUtil;
+import org.jdom.Element;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.annotation.concurrent.ThreadSafe;
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
+import static com.intellij.openapi.util.text.StringUtil.isNotEmpty;
+import static java.util.Collections.emptyMap;
 import static java.util.stream.Collectors.toList;
 
 @ThreadSafe
@@ -30,12 +32,59 @@ public class TermsOfServiceManagerImpl implements TermsOfServiceManager {
     @NotNull
     private final TimeService timeService;
 
+    private final List<Agreement> myAgreements = new ArrayList<>();
+    private final List<ExternalAgreementLink> externalAgreements = new ArrayList<>();
+    private volatile GuestNoticeSettings myGuestNotice = null;
+
     public TermsOfServiceManagerImpl(@NotNull TermsOfServiceConfig config,
                                      @NotNull UserModel userModel,
                                      @NotNull TimeService timeService) {
         myConfig = config;
         this.userModel = userModel;
         this.timeService = timeService;
+        config.setOnChange(this::reloadConfig);
+    }
+
+    private void reloadConfig(@NotNull Element config) {
+        myAgreements.clear();
+        externalAgreements.clear();
+
+        for (Object agreementEl : config.getChildren("agreement")) {
+            Element paramsElement = ((Element) agreementEl).getChild("parameters");
+            Map<String, String> params = paramsElement == null ? emptyMap() : XmlUtil.readParameters(paramsElement);
+            if (params.get("agreement-file") != null) {
+                List<TermsOfServiceManager.Consent> consents = new ArrayList<>();
+                Element consentsEl = ((Element) agreementEl).getChild("consents");
+                if (consentsEl != null) {
+                    for (Object consent : consentsEl.getChildren("consent")) {
+                        Element consentEl = ((Element) consent);
+                        String id = consentEl.getAttributeValue("id");
+                        String text = consentEl.getAttributeValue("text");
+                        boolean checked = Boolean.parseBoolean(consentEl.getAttributeValue("checked"));
+                        if (isNotEmpty(id) && isNotEmpty(text)) {
+                            consents.add(new ConsentSettings(id, text, checked));
+                        }
+                    }
+                }
+                AgreementImpl agreement = new AgreementImpl(((Element) agreementEl).getAttributeValue("id"), params, consents);
+                myAgreements.add(agreement);
+            }
+        }
+
+        for (Object agreementEl : config.getChildren("externalAgreementLink")) {
+            externalAgreements.add(new ExternalAgreementLinkSettings(((Element) agreementEl).getAttributeValue("text"), ((Element) agreementEl).getAttributeValue("url")));
+        }
+
+        GuestNoticeSettings guestNoticeSettings = null;
+        Element guestNoticeEl = config.getChild("guest-notice");
+        if (guestNoticeEl != null) {
+            Element paramsElement = guestNoticeEl.getChild("parameters");
+            Map<String, String> params = paramsElement == null ? emptyMap() : XmlUtil.readParameters(paramsElement);
+            if (params.get("agreement") != null && params.get("text") != null) {
+                guestNoticeSettings = new GuestNoticeSettings(params.get("text"), params.get("agreement"));
+            }
+        }
+        myGuestNotice = guestNoticeSettings;
     }
 
     @NotNull
@@ -52,127 +101,206 @@ public class TermsOfServiceManagerImpl implements TermsOfServiceManager {
     }
 
     @NotNull
-    public List<Agreement> getAgreements() {
-        return myConfig.getAgreements().stream().map(AgreementImpl::new).collect(toList());
+    public synchronized List<Agreement> getAgreements() {
+        return new ArrayList<>(myAgreements);
     }
 
     @NotNull
     @Override
-    public Optional<Agreement> findAgreement(@NotNull String id) {
-        return getAgreements().stream().filter(a -> a.getId().equals(id)).findFirst();
+    public synchronized Optional<Agreement> findAgreement(@NotNull String id) {
+        return myAgreements.stream().filter(a -> a.getId().equals(id)).findFirst();
     }
 
     @NotNull
     @Override
-    public List<ExternalAgreementLink> getExternalAgreements() {
-        return myConfig.getExternalAgreements();
+    public synchronized List<ExternalAgreementLink> getExternalAgreements() {
+        return new ArrayList<>(externalAgreements);
     }
 
     @NotNull
     @Override
     public Optional<GuestNotice> getGuestNotice() {
-        Optional<TermsOfServiceConfig.GuestNoticeSettings> guestNotice = myConfig.getGuestNotice();
-        if (!guestNotice.isPresent()) {
+        if (myGuestNotice == null) {
             return Optional.empty();
         }
 
-        Optional<TermsOfServiceConfig.AgreementSettings> agreement = myConfig.getAgreement(guestNotice.get().getAgreementId());
+        Optional<Agreement> agreement = findAgreement(myGuestNotice.getAgreementId());
 
-        if (agreement.isPresent()) {
-            return guestNotice.map(s -> new GuestNotice() {
-                @NotNull
-                @Override
-                public String getText() {
-                    return s.getText();
-                }
+        return agreement.map(a -> new GuestNotice() {
+            @NotNull
+            @Override
+            public String getText() {
+                return myGuestNotice.getText();
+            }
 
-                @NotNull
-                @Override
-                public String getLink() {
-                    return new AgreementImpl(agreement.get()).getLink();
-                }
-            });
-        } else {
-            return Optional.empty();
-        }
+            @NotNull
+            @Override
+            public String getLink() {
+                return a.getLink();
+            }
+        });
     }
 
     private final class AgreementImpl implements Agreement {
 
-        private final TermsOfServiceConfig.AgreementSettings agreementSettings;
+        private final String id;
+        private final Map<String, String> params;
+        private final List<Consent> consents;
 
-        private AgreementImpl(TermsOfServiceConfig.AgreementSettings agreementSettings) {
-            this.agreementSettings = agreementSettings;
+        AgreementImpl(@NotNull String id, @NotNull Map<String, String> params, @NotNull List<Consent> consents) {
+            this.id = id;
+            this.params = params;
+            this.consents = consents;
         }
 
         @NotNull
         @Override
         public String getId() {
-            return agreementSettings.getId();
+            return id;
         }
 
         @NotNull
         @Override
         public String getShortName() {
-            return agreementSettings.getShortName();
+            return StringUtil.notNullize(params.get("short-name"), "Terms of Service");
         }
 
         @NotNull
         @Override
         public String getFullName() {
-            return agreementSettings.getFullName();
+            return StringUtil.notNullize(params.get("full-name"), "Terms of Service");
         }
 
         @Nullable
         @Override
         public String getText() {
-            return agreementSettings.getText();
+            String agreementFileParam = params.get("agreement-file");
+            if (agreementFileParam == null) {
+                return null;
+            }
+            File agreementFile = myConfig.getConfigFile(agreementFileParam);
+            try {
+                return FileUtil.readText(agreementFile, "UTF-8");
+            } catch (IOException e) {
+                TermsOfServiceLogger.LOGGER.warnAndDebugDetails("Error while reading Terms Of Service agreement file from " + agreementFile, e);
+                throw new IllegalStateException("Error while reading Terms Of Service agreement file from " + agreementFile, e);
+            }
         }
 
         @NotNull
         @Override
         public String getLink() {
-            return agreementSettings.getLink() != null ?
-                    agreementSettings.getLink() :
-                    ViewTermsOfServiceController.PATH + "?agreement=" + agreementSettings.getId();
+            return ViewTermsOfServiceController.PATH + "?agreement=" + id;
         }
 
         @NotNull
         @Override
         public List<Consent> getConsents() {
-            return agreementSettings.getConsents();
+            return consents;
         }
 
         @Override
         public boolean isAccepted(@NotNull SUser user) {
-            String acceptedVersion = user.getPropertyValue(new SimplePropertyKey("teamcity.policy." + agreementSettings.getId() + ".acceptedVersion"));
-            return acceptedVersion != null && VersionComparatorUtil.compare(acceptedVersion, agreementSettings.getVersion()) >= 0;
+            String acceptedVersion = user.getPropertyValue(new SimplePropertyKey("teamcity.policy." + id + ".acceptedVersion"));
+            return acceptedVersion != null && VersionComparatorUtil.compare(acceptedVersion, params.getOrDefault("version", "1")) >= 0;
         }
 
         @Override
         public void accept(@NotNull SUser user, @NotNull HttpServletRequest request) {
-            user.setUserProperty(new SimplePropertyKey("teamcity.policy." + agreementSettings.getId() + ".acceptedVersion"), agreementSettings.getVersion());
-            user.setUserProperty(new SimplePropertyKey("teamcity.policy." + agreementSettings.getId() + ".acceptedDate"), new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ").format(timeService.now()));
-            user.setUserProperty(new SimplePropertyKey("teamcity.policy." + agreementSettings.getId() + ".acceptedFromIP"), WebUtil.getRemoteAddress(request));
+            user.setUserProperty(new SimplePropertyKey("teamcity.policy." + id + ".acceptedVersion"), params.getOrDefault("version", "1"));
+            user.setUserProperty(new SimplePropertyKey("teamcity.policy." + id + ".acceptedDate"), new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ").format(timeService.now()));
+            user.setUserProperty(new SimplePropertyKey("teamcity.policy." + id + ".acceptedFromIP"), WebUtil.getRemoteAddress(request));
         }
 
         @Override
         public void changeConsentState(@NotNull SUser user, @NotNull String consentId, boolean agreed, @NotNull HttpServletRequest request) {
             if (agreed) {
-                user.setUserProperty(new SimplePropertyKey("teamcity.policy." + agreementSettings.getId() + ".consent." + consentId + ".accepted"), "true");
-                user.setUserProperty(new SimplePropertyKey("teamcity.policy." + agreementSettings.getId() + ".consent." + consentId + ".acceptedDate"), new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ").format(timeService.now()));
-                user.setUserProperty(new SimplePropertyKey("teamcity.policy." + agreementSettings.getId() + ".consent." + consentId + ".acceptedFromIP"), WebUtil.getRemoteAddress(request));
+                user.setUserProperty(new SimplePropertyKey("teamcity.policy." + id + ".consent." + consentId + ".accepted"), "true");
+                user.setUserProperty(new SimplePropertyKey("teamcity.policy." + id + ".consent." + consentId + ".acceptedDate"), new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ").format(timeService.now()));
+                user.setUserProperty(new SimplePropertyKey("teamcity.policy." + id + ".consent." + consentId + ".acceptedFromIP"), WebUtil.getRemoteAddress(request));
             } else {
-                user.deleteUserProperty(new SimplePropertyKey("teamcity.policy." + agreementSettings.getId() + ".consent." + consentId + ".accepted"));
-                user.deleteUserProperty(new SimplePropertyKey("teamcity.policy." + agreementSettings.getId() + ".consent." + consentId + ".acceptedDate"));
-                user.deleteUserProperty(new SimplePropertyKey("teamcity.policy." + agreementSettings.getId() + ".consent." + consentId + ".acceptedFromIP"));
+                user.deleteUserProperty(new SimplePropertyKey("teamcity.policy." + id + ".consent." + consentId + ".accepted"));
+                user.deleteUserProperty(new SimplePropertyKey("teamcity.policy." + id + ".consent." + consentId + ".acceptedDate"));
+                user.deleteUserProperty(new SimplePropertyKey("teamcity.policy." + id + ".consent." + consentId + ".acceptedFromIP"));
             }
         }
 
+
         @Override
         public String toString() {
-            return "Agreement " + agreementSettings.getShortName() + " (id = " + agreementSettings.getId() + ")";
+            return "Agreement " + StringUtil.notNullize(params.get("short-name"), "Terms of Service") + " (id = " + id + ")";
+        }
+
+    }
+
+    class ExternalAgreementLinkSettings implements TermsOfServiceManager.ExternalAgreementLink {
+        private final String text;
+        private final String url;
+
+        ExternalAgreementLinkSettings(String text, String url) {
+            this.text = text;
+            this.url = url;
+        }
+
+        @NotNull
+        @Override
+        public String getName() {
+            return text;
+        }
+
+        @NotNull
+        @Override
+        public String getUrl() {
+            return url;
         }
     }
 
+    class GuestNoticeSettings {
+        private final String text;
+        private final String agreementId;
+
+        GuestNoticeSettings(String text, String agreementId) {
+            this.text = text;
+            this.agreementId = agreementId;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        public String getAgreementId() {
+            return agreementId;
+        }
+    }
+
+    class ConsentSettings implements TermsOfServiceManager.Consent {
+        @NotNull
+        private final String id;
+        @NotNull
+        private final String text;
+        private final boolean checked;
+
+        ConsentSettings(@NotNull String id, @NotNull String text, boolean checked) {
+            this.id = id;
+            this.text = text;
+            this.checked = checked;
+        }
+
+        @NotNull
+        @Override
+        public String getId() {
+            return id;
+        }
+
+        @Override
+        public boolean isCheckedByDefault() {
+            return checked;
+        }
+
+        @NotNull
+        @Override
+        public String getText() {
+            return text;
+        }
+    }
 }
